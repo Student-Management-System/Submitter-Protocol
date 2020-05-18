f6e0d9c6e511eb6569e5dce191c76f3db28a2873
Sascha El-Sharkawy <elscha@sse.uni-hildesheim.de>
2020-05-18 13:42:55 +0200

    Finishes merge and integrated all changes from Tim

--- src/main/java/net/ssehub/exercisesubmitter/protocol/backend/ReviewerProtocol.java
+++ src/main/java/net/ssehub/exercisesubmitter/protocol/backend/ReviewerProtocol.java
@@ -6,7 +6,9 @@ import java.util.List;
 import net.ssehub.exercisesubmitter.protocol.backend.DataNotFoundException.DataType;
 import net.ssehub.studentmgmt.backend_api.ApiException;
 import net.ssehub.studentmgmt.backend_api.api.AssessmentsApi;
+import net.ssehub.studentmgmt.backend_api.model.AssessmentCreateDto;
 import net.ssehub.studentmgmt.backend_api.model.AssessmentDto;
+import net.ssehub.studentmgmt.backend_api.model.PartialAssessmentDto;
 
 /**
  * Network protocol designed for the &quot;Exercise Reviewer&quot;.
@@ -75,4 +77,70 @@ public class ReviewerProtocol extends NetworkProtocol {
         return singleAssessment;
     }
     
+    /**
+     * Creates the Assessment for an Assignment.
+     * @param body The Assessment body.
+     * @param assignmentId The id of the specified assignment.
+     * @throws NetworkException when network problems occur.
+     */
+    public void createAssessment(AssessmentCreateDto body,String assignmentId) throws NetworkException {
+        try {
+            apiAssessments.createAssessment(body, super.getCourseID(), assignmentId);
+        } catch (IllegalArgumentException e) {
+            throw new ServerNotFoundException(e.getMessage(), basePath);
+        } catch (ApiException e) {
+            throw new DataNotFoundException("Assessmentbody not found", courseName, DataType.ASSESSMENT_BODY_NOT_FOUND);
+        }
+    }
+    
+    /**
+     * Creates a Partial Assessment.
+     * @param body The body of the partial assessment
+     * @param assignmentId The id of the specified assignment.
+     * @param assessmentId The id of the specified assessment.
+     * @throws NetworkException when network problems occur.
+     */
+    public void createPartialAssessment(PartialAssessmentDto body, String assignmentId, String assessmentId) 
+            throws NetworkException {
+        try {
+            apiAssessments.addPartialAssessment(body, super.getCourseID(), assignmentId, assessmentId);
+        } catch (IllegalArgumentException e) {
+            throw new ServerNotFoundException(e.getMessage(), basePath);
+        } catch (ApiException e) {
+            throw new DataNotFoundException("Assessmentbody not found", courseName, DataType.ASSESSMENT_BODY_NOT_FOUND);
+        }
+    }
+    
+    /**
+     * Updates an Assessment.
+     * @param body The body of the assessment that is updated.
+     * @param assignmentId The id of the specified assignment.
+     * @param assessmentId The id of the specified assessment.
+     * @throws NetworkException when network problems occur.
+     */
+    public void updateAssessment(AssessmentDto body, String assignmentId, String assessmentId) throws NetworkException {
+        try {
+            apiAssessments.updateAssessment(body, super.getCourseID(), assignmentId, assessmentId);
+        } catch (IllegalArgumentException e) {
+            throw new ServerNotFoundException(e.getMessage(), basePath);
+        } catch (ApiException e) {
+            throw new DataNotFoundException("Assessmentbody not found", courseName, DataType.ASSESSMENT_BODY_NOT_FOUND);
+        }
+    }
+    
+    /**
+     * Deletes an Assessment.
+     * @param assignmentId The id of the specified assignment.
+     * @param assessmentId The id of the specified assessment.
+     * @throws NetworkException when network problems occur.
+     */
+    public void deleteAssessment(String assignmentId, String assessmentId) throws NetworkException {
+        try {
+            apiAssessments.deleteAssessment(super.getCourseID(), assignmentId, assessmentId);
+        } catch (IllegalArgumentException e) {
+            throw new ServerNotFoundException(e.getMessage(), basePath);
+        } catch (ApiException e) {
+            throw new DataNotFoundException("Assessment not found", courseName, DataType.ASSESSMENTS_NOT_FOUND);
+        }
+    }
 }
