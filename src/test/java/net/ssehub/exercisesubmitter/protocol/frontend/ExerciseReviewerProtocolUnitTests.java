da951343c3034683d4180160a9f33e72d64617d2
TimKuno <tim.kunold@gmx.de>
2020-07-01 14:01:46 +0200

    initial commit

---
+++ src/test/java/net/ssehub/exercisesubmitter/protocol/frontend/ExerciseReviewerProtocolUnitTests.java
@@ -0,0 +1,24 @@
+package net.ssehub.exercisesubmitter.protocol.frontend;
+
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Tests the {@link ExerciseReviewerProtocol} <b>without</b> querying the REST server.
+ * @author Kunold
+ *
+ */
+public class ExerciseReviewerProtocolUnitTests {
+
+    /**
+     *  Tests if the returned String with the group and review informations is correct formated.
+     */
+    @Disabled
+    @Test
+    public void testGetSubmissionReviews() {
+        
+    }
+
+}
